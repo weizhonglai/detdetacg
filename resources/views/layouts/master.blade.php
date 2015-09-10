3009df1dc28f276d63b37aa15d65aae2b83466f1
Louis Tan <louistan3s@gmail.com>
2015-09-11 00:13:21 +0800

    Editing Master Page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,46 +19,56 @@
 @yield('css_include')
 </head>
 <body>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/index">DetDetACG</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">About Us <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">General help</a></li>
+            <li><a href="#">Posts</a></li>
+          </ul>
+        </li>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Pages<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/index">Home</a></li>
+            <li><a href="/categories">Categories</a></li>
+            <li><a href="/listing">Listing</a></li>
+            <li><a href="/prod_detail">Product Details</a></li>
+            <li><a href="/cart">Cart</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/sign-up">Register</a></li>
+          </ul>
+        </li>
+        <li><a href="#">How to Buy</a></li> 
+        <li><a href="#">FAQs</a></li> 
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Bag</span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/cart">5 items</a></li>
+            <li><a href="/cart">$ 150</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#myModal" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <div id="wrapper" ng-controller="GlobalController">
 	<!-- Header Start -->
-    <div id="header">
-    	<!-- Logo Start -->
-        <div class="logo">
-        	<a href="index">&nbsp;<span class="dis_none">Estore 3</span></a>
+     <div id="slider">
+            <a href="#"><img src="/templates/images/01.jpg" alt="" /></a>
+            <a href="#"><img src="/templates/images/02.jpg" alt="" /></a>
+            <a href="#"><img src="/templates/images/03.jpg" alt="" /></a>
+            <a href="#"><img src="/templates/images/04.jpg" alt="" /></a>
+            <a href="#"><img src="/templates/images/05.jpg" alt="" /></a>
         </div>
-        <!-- Logo End -->
-        <!-- Cart and Top Navigation Section Start -->
-        <div class="cart_topnavi">
-        	<!-- Cart Tab Start -->
-        	<div class="cart_tab">
-            	<div class="left_curv">&nbsp;</div>
-                <div class="center_curv">
-                	<ul>
-                    	<li class="bag bold"><a href="/cart">Shopping Bag</a></li>
-                        <li class="items"><a href="/cart">5 items</a></li>
-                        <li class="price"><a href="/cart">$ 150</a></li>
-                    </ul>
-                </div>
-                <div class="right_curv">&nbsp;</div>
-            </div>
-            <div class="clear"></div>
-            <!-- Cart Tab End -->
-            <div class="topnavi">
-            	<ul>
-                	<li><a href="#">My Account</a></li>
-                    <li><a href="#">My Wishlist</a></li>
-                    <li><a href="/cart">My Cart</a></li>
-                    <li><a href="#">Checkout</a></li>
-                    <li class="last"><a href="#myModal" data-toggle="modal" data-target="#myModal">Log In</a>
-                    	
-
-                    </li>
-                </ul>
-            </div>
-            <!-- Cart Tab End -->
-        </div>
-        <!-- Cart and Top Navigation Section Start -->
-    </div>
     <!-- Modal -->
         <div class="modal fade" id="myModal" role="dialog" ng-controller="SignInController">
         <div class="modal-dialog modal-md">
@@ -67,79 +77,25 @@
               <button type="button" class="close" data-dismiss="modal">&times;</button>
               <h4 class="modal-title">Login</h4>
             </div>
-           
             <div class="modal-body">
                 <div class="container">
                 <h2>Login</h2>
-
                         <p><input type="text" class="span3 username" name="username" id="username" placeholder="Username"></p>
                         <p><input type="password" class="span3 password" name="password" id="password" placeholder="Password"></p>
                         <p><button type="submit" class="btn btn-primary" ng-click="signIn()">Sign in</button>
                         <a href="#">Forgot Password?</a>
-                        </p>
-                 
+                        </p>  
                 </div>
                 <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
             </div>
             </div>
-            
           </div>
         </div>
     <!-- Header End -->
-    <!-- Navi Start -->
-    <div id="navi">
-    	<!-- Navigation Start -->
-    	<div class="navigation">
-        	<div id="smoothmenu1" class="ddsmoothmenu">
-                <ul>
-                    <li class="icon"><a href="/index">&nbsp;<span class="dis_none">Home</span></a></li>
-                    <li><a href="#">About Us</a>
-                        <ul>
-                            <li><a href="#">General help</a></li>
-                            <li><a href="#">Posts</a></li>
-                            <li><a class="last" href="#">Pages</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">How to Buy</a></li>
-                    <li><a href="#">FAQs</a></li>
-                    <li><a href="#">Pages</a>
-                        <ul>
-                            <li><a href="#" class="last">Colors</a>
-                                <ul>
-                                    <li><a href="javascript:chooseStyle('none', 60)">Gray</a></li>
-                                    <li><a href="javascript:chooseStyle('blue', 60)">Blue</a></li>
-                                    <li><a href="javascript:chooseStyle('red', 60)">Red</a></li>
-                                    <li><a href="javascript:chooseStyle('orange', 60)">Orange</a></li>
-                                    <li><a href="javascript:chooseStyle('green', 60)">Green</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="/index">Home</a></li>
-                            <li><a href="/categories">Categories</a></li>
-                            <li><a href="/listing">Listing</a></li>
-                            <li><a href="/prod_detail">Product Details</a></li>
-                            <li><a href="/cart">Cart</a></li>
-                            <li><a href="/login">Login</a></li>
-                            <li><a href="/sign-up">Register</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="/contact">Contact Us</a></li>
-                </ul>
-				<div class="clear"></div>
-			</div>
-        </div>
-        <!-- Navigation End -->
-    </div>
-    <!-- Navi End -->
     <!-- Banner Start -->
-        <div id="slider">
-            <a href="#"><img src="/templates/images/01.jpg" alt="" /></a>
-            <a href="#"><img src="/templates/images/02.jpg" alt="" /></a>
-            <a href="#"><img src="/templates/images/03.jpg" alt="" /></a>
-            <a href="#"><img src="/templates/images/04.jpg" alt="" /></a>
-            <a href="#"><img src="/templates/images/05.jpg" alt="" /></a>
-        </div>
+       
     <!-- Navi End -->
      <!-- Content Start -->
         <div id="content">
