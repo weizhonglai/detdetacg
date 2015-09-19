cf2690b7f1356b0ad5cfc25468aa44ecd4bd8208
Louis Tan <louistan3s@gmail.com>
2015-09-19 17:34:20 +0800

    Editing Frontend

--- resources/views/components/menu-left.blade.php
+++ resources/views/components/menu-left.blade.php
@@ -1,9 +1,7 @@
 @section('menu-left')
 <!-- Left Column Start-->
-<div class="left_colmn">
-	<!-- Categories Section -->
-	<div class="section">
-    	<h4>Categories</h4>
+<div class="section">
+      <h4>Categories</h4>
         <div class="glossymenu">
             <a class="menuitem submenuheader" href="categories" >Bracelets</a>
             <div class="submenu">
@@ -77,14 +75,6 @@
                     <li><a href="listing">Diamond Set</a></li>
                 </ul>
             </div>
-    	</div>
+      </div>
     </div>
-	<!-- Advertise Section -->
-    <div class="section margn_end">
-        <a href="#"><img src="/templates/images/ad.gif" class="adv" alt="" /><br /></a>
-        <a href="#" class="readmore bold">Read More</a>
-        <div class="clear"></div>
-    </div>
-</div>
-            <!-- Left Column End-->
 @stop
