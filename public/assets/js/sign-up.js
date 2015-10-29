03ba045e9495247d0da9ddd9aa9f1f6e2ea9298f
vanese <vaneselim88@gmail.com>
2015-10-29 12:33:25 +0800

    edit sign-up

--- public/assets/js/sign-up.js
+++ public/assets/js/sign-up.js
@@ -2,14 +2,67 @@ var App = App || angular.module('detdetApp', []);
 
 App.controller('MainController', function($scope, $http){
 
+    $scope.init = function() {        
+        //BEGIN PLUGINS DATE RANGE PICKER
+        $('input[name="daterangepicker-default"]').daterangepicker();
+        $('input[name="daterangepicker-date-time"]').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' });
+        $('.reportrange').daterangepicker(
+            {
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                    'Last 7 Days': [moment().subtract('days', 6), moment()],
+                    'Last 30 Days': [moment().subtract('days', 29), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+                },
+                startDate: moment().subtract('days', 29),
+                endDate: moment(),
+                opens: 'left'
+            },
+            function(start, end) {
+                $('.reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+                $('input[name="datestart"]').val(start.format("YYYY-MM-DD"));
+                $('input[name="endstart"]').val(end.format("YYYY-MM-DD"));
+            }
+        );
+        $('.reportrange span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+        //END PLUGINS DATE RANGE PICKER
+
+        //BEGIN PLUGINS DATE PICKER
+        $('.datepicker-default').datepicker();
+        $('.datepicker-years').datepicker({
+            startView: 1,
+            minViewMode: 2
+        });
+        $('.input-daterange').datepicker({
+            format: "yyyy-mm-dd"
+        });
+        $('.datepicker-inline').datepicker({
+            format: "yyyy-mm-dd",
+            startView: 2,
+            minViewMode: 1
+        });
+        //END PLUGINS DATE PICKER
+
+        // BEGIN PLUGIN MASK INPUT
+        $("#date").mask("999999-99-9999");
+        $("#phone").mask("(999) 999-9999");
+        $("#product-key").mask("(aa) 99-999")
+        // END PLUGIN MASK INPUT
+    }
+
     $scope.signUp = function() {
         
         var first_name = angular.element("input.first_name").val(),
             last_name = angular.element("input.last_name").val(),  
-            domain = angular.element("input.mail_domain").val(),   
+            domain = angular.element("input.mail_domain").val(), 
+
             mail = angular.element("select.mail_sys").val(),   
             email = domain + '@' + mail, 
+            
             nric = angular.element("input.nric").val(),
+            passport = angular.element("input.passport").val(),
             dob = angular.element("input.dob").val(),
             gender = angular.element(".option_gender input:checked").val(),
             address1 = angular.element("input.address1").val(),
@@ -17,17 +70,40 @@ App.controller('MainController', function($scope, $http){
             post_code = angular.element("input.post_code").val(),
             city   = angular.element("input.city").val(),
             state = angular.element("select.state").val(),
-            mobile = angular.element("input.mobile").val(),
-            home_number = angular.element("input.home_number").val(),
-            office_number = angular.element("input.office_number").val(),
+
+            phone_area_code = angular.element("input.phone_area_code").val(),
+            phone_num = angular.element("input.phone_num").val(),
+            mobile = '01' + phone_area_code + '-' + phone_num;
+
+            home_area_code = angular.element("input.home_area_code").val(),
+            home_num = angular.element("input.home_num").val(),
+            home_number = '0' + home_area_code + '-' + home_num;
+
+            office_area_code = angular.element("input.office_area_code").val(),
+            office_num = angular.element("input.office_num").val(),
+            office_number = '0' + office_area_code + '-' + office_num;
+
             fax_number = angular.element("input.fax_number").val(),
             first_password = angular.element("input.first_password").val(),
             confirm_password = angular.element("input.confirm_password").val();
-
-        if (first_name == '' || last_name == '' || mail == ''|| nric == '' || dob == ''|| mobile == '' || first_password == '' || confirm_password == '') {
+        
+        if (first_name == '' || last_name == '' || mail == ''|| nric == '' || passport == '' || dob == '' || first_password == '' || confirm_password == '') {
             alert('You have to fill up all fields to complete the sign up');
             return;
         }    
+
+        if(passport.length < 9){
+            alert('passport have to be at least 9 characters.');    
+            return;
+        } 
+
+        if(phone_area_code == '' && phone_num == '') {
+            if(office_area_code == '' && office_num == '') {
+                if(home_area_code == '' && home_num == '') {
+                    alert('You have to fill up at least one phone number');
+                }
+            }
+        }
 
         if (!/^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/.test(email)) {
             alert('Invalid email address.');
--- resources/views/sign-up.blade.php
+++ resources/views/sign-up.blade.php
@@ -6,12 +6,18 @@
 @stop
 
 @section('javascript_include')
+<script src="/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/templates/admin/vendors/moment/moment.js"></script>
+<script src="/templates/admin/vendors/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+<script src="/templates/admin/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="/templates/admin/vendors/jquery-maskedinput/jquery-maskedinput.js"></script>
+
 <script src="/assets/js/sign-up.js"></script>
 @stop
 
 @section('content')
     <!-- Right Column Start-->
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8" data-ng-init="init()">
         <div class="right_colmn">           
             <!-- Register Section -->
             <div class="register">
@@ -22,10 +28,10 @@
                     	<li class="txt">Name</li>
                     	<li class="inputfield">
                             <div class="row">
-                                <div class="col-xs-6">
+                                <div class="col-xs-6" style="padding-right:0px">
                                     <input type="text" class="first_name" style="width: 90%" placeholder="First Name"/>
                                 </div>
-                                <div class="col-xs-6">
+                                <div class="col-xs-6" style="padding-left:0px">
                                     <input type="text" class="last_name" style="width: 90%" placeholder="Last Name" />
                                 </div>
                             </div>
@@ -42,13 +48,21 @@
                     <div class="clear"></div>
                     <ul class="forms">
                         <li class="txt">Date of Birth</li>
-                        <li class="inputfield"><input type="text" class="dob" /></li>
+                        <li class="inputfield"><input type="text" data-date-format="yyyy-mm-dd" class="dob datepicker-default" placeholder="yyyy-mm-dd" />
+                        </li>
                         <li class="req">(Required)</li>
                     </ul>
                     <div class="clear"></div>
                     <ul class="forms">
                         <li class="txt">NRIC</li>
-                        <li class="inputfield"><input type="text" class="nric" placeholder="123456-12-1234"/></li>
+                        <li class="inputfield"><input type="text" id="date" class="nric" placeholder="______-__-____"/>
+                        </li>
+                        <li class="req">(Required)</li>
+                    </ul>
+                    <div class="clear"></div>
+                    <ul class="forms">
+                        <li class="txt">Passport Num</li>
+                        <li class="inputfield"><input type="text" class="passport" /></li>
                         <li class="req">(Required)</li>
                     </ul>
                     <div class="clear"></div>
@@ -57,10 +71,10 @@
                         <li class="inputfield">
                             <div class="row">
                                 <div class="col-xs-7">
-                                    <input type="text" class="mail_domain" style="width: 90%"/> @
+                                    <input type="text" class="mail_domain" style="width: 90%"/>&nbsp;&nbsp;@
                                 </div>
-                                <div class="col-xs-5">
-                                    <select class="mail_sys" style="height: 21px; width: 90%">
+                                <div class="col-xs-5" style="padding-left:0px">
+                                    <select class="mail_sys" style="height: 21px; width: 90%; ">
                                         <option>gmail.com</option>
                                         <option>hotmail.com</option>
                                     </select>
@@ -77,31 +91,27 @@
                     <ul class="forms">  
                     	<li class="txt">Street Address 1</li>
                         <li class="inputfield"><input type="text" class="address1" /></li>
-                        <li class="req">(Required)</li>
                     </ul>
                     <div class="clear"></div>
                     <ul class="forms">
                         <li class="txt">Street Address 2</li>
                         <li class="inputfield"><input type="text" class="address2" /></li>
-                        <li class="req">(Required)</li>
                     </ul>
                     <div class="clear"></div>
                     <ul class="forms">
                     	<li class="txt">Post Code</li>
                     	<li class="inputfield"><input type="text" class="post_code" /></li>
-                        <li class="req">(Required)</li>
                     </ul>
                     <div class="clear"></div>
                     <ul class="forms">
                     	<li class="txt">City</li>
                     	<li class="inputfield"><input type="text" class="city" /></li>
-                        <li class="req">(Required)</li>
                     </ul>
                     <div class="clear"></div>
                     <ul class="forms">
                     	<li class="txt">State/Province</li>
-                    	<li>
-                            <select class="state" style="height: 21px; width: 100%">
+                    	<li class="inputfield">
+                            <select class="state" style="height: 21px; width: 96%">
                                 <option>Johor</option>
                                 <option>Kedah</option>
                                 <option>Kelantan</option>
@@ -117,7 +127,6 @@
                                 <option>Terengganu</option>
                             </select>
                         </li>
-                        <li class="req">(Required)</li>
                     </ul>
                     <div class="clear"></div>
                 </div>
@@ -125,22 +134,50 @@
                 	<h3>Your Contact Information</h3>
                     <ul class="forms">
                     	<li class="txt">Phone Number</li>
-                    	<li class="inputfield"><input type="text" class="mobile" /></li>
-                        <li class="req">(Required)</li>
+                    	<li class="inputfield">
+                            <div class="row">
+                                <div class="col-xs-2">
+                                    <span style="position: absolute">01</span><input type="text" class="phone_area_code" style="width: 100%; margin-left: 18px" />
+                                </div>
+                                <div class="col-xs-10">
+                                    <span style="position: absolute; left: 6px"> - </span><input type="text" class="phone_num" style="width: 95%;" />
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                     <ul class="forms">
                     	<li class="txt">Home Number</li>
-                    	<li class="inputfield"><input type="text" class="home_number" /></li>
+                    	<li class="inputfield">
+                            <div class="row">
+                                <div class="col-xs-2">
+                                    <span style="position: absolute">0</span><input type="text" class="home_area_code" style="width: 100%; margin-left: 15px" />
+                                </div>
+                                <div class="col-xs-10">
+                                    <span style="position: absolute; left: 5px"> - </span><input type="text" class="home_num" style="width: 95%;" />
+                                </div>
+                            </div>
+                        </li>
                         <li class="req"></li>
                     </ul>
                     <ul class="forms">
                         <li class="txt">Office Number</li>
-                        <li class="inputfield"><input type="text" class="office_number" /></li>
+                        <li class="inputfield">
+                            <div class="row">
+                                <div class="col-xs-2">
+                                    <span style="position: absolute">0</span><input type="text" class="office_area_code" style="width: 100%; margin-left: 15px" />
+                                </div>
+                                <div class="col-xs-10">
+                                    <span style="position: absolute; left: 5px"> - </span><input type="text" class="office_num" style="width: 95%;" />
+                                </div>
+                            </div>
+                        </li>
                         <li class="req"></li>
                     </ul>
                     <ul class="forms">
                         <li class="txt">Fax Number</li>
-                        <li class="inputfield"><input type="text" class="fax_number" /></li>
+                        <li class="inputfield">
+                            <li class="inputfield"><input type="text" class="fax_number" /></li>
+                        </li>
                         <li class="req"></li>
                     </ul>
                     <div class="clear"></div>
