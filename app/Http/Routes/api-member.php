be69d2156abdd8fd9163e06cb1ff48a1b0d3040e
Lai Wei Zhong <weizhong.lai@webuiltit.com.my>
2015-10-30 14:47:27 +0800

    update fetch amount

--- app/Http/Routes/api-member.php
+++ app/Http/Routes/api-member.php
@@ -2,5 +2,6 @@
 
 Route::group(['prefix' => 'api/member'], function(){
 	Route::post('/top-up/request', 'CmsController@topUpRequest');
+	Route::get('/top-up/list', 'ProfileController@fetchTopupAmount');
 });
 
